c2154dceee28bd2608e6017ef70946a444b3d7fd
sohanpro <shsohan47@gmail.com>
2023-06-26 11:55:15 +0600

    department add member automation fix

--- department.cy.js
+++ department.cy.js
@@ -44,124 +44,151 @@ cy.url().should('eq', 'https://tmsv2.aamarpay.dev/profile');
 
 
 
-//   it('Check department data ',()=>{
-//     cy.contains('Department').scrollIntoView().click({force:true});
-//     // Assuming you want to find a button with a specific href value
+  it('Check department data ',()=>{
+    cy.contains('Department').scrollIntoView().click({force:true});
+    // Assuming you want to find a button with a specific href value
 
-// // Find the button using the href attribute
-// cy.get('a[href="/department"]').click({force:true});
-// cy.wait(3000);
-//     cy.get('.table-responsive.table-border-radius')
-//     .find('tbody>tr')
-//     .each((row)=>{
+// Find the button using the href attribute
+cy.get('a[href="/department"]').click({force:true});
+cy.wait(3000);
+    cy.get('.ant-table')
+    .find('tbody>tr')
+    .each((row)=>{
  
-//   cy.wrap(row)
-//   .find('td').last()
-//   .find('.btn-link.dd-caret-none.dropdown-toggle').click({force:true})
-//   cy.get('.dropdown-menu.dropdown-menu-right.p-10.ms-4.dropdown-menu.show').contains('Edit').click({force:true});
-//   cy.contains('Cancel').click({force:true})
-//   //cy.wait(3000)
-//     })
+  cy.wrap(row)
+  .find('td').last()
+  .find('.btn-link.dd-caret-none.dropdown-toggle').click({force:true})
+  cy.get('.dropdown-menu.dropdown-menu-right.p-10.ms-4.dropdown-menu.show').contains('Edit').click({force:true});
+  cy.contains('Cancel').click({force:true})
+  //cy.wait(3000)
+    })
    
 
-//     //pagination
-//     cy.get('.pagination-container')
+    //pagination
+   // cy.get('.pagination-container')
   
-//   cy.get('select[name="rows"]').find('option').each(($option) => {
-//     const optionText = $option.text();
-//     cy.get('select[name="rows"]').select(optionText,{force:true,delay:1000});
+  // cy.get('select[name="rows"]').find('option').each(($option) => {
+  //   const optionText = $option.text();
+  //   cy.get('select[name="rows"]').select(optionText,{force:true,delay:1000});
     
 
-//     cy.get('select[name="rows"]').should('have.value', $option.val());
-//     cy.get('.table-responsive.table-border-radius tbody tr').its('length').as('numberOfRows')
+  //   cy.get('select[name="rows"]').should('have.value', $option.val());
+  //   cy.get('.table-responsive.table-border-radius tbody tr').its('length').as('numberOfRows')
     
     
-//   });
-//   })
+  // });
+  })
   //Add department panel
-  // it('check Add department',()=>
-  // {
-  //   cy.contains('Add Department').scrollIntoView().click({force:true});
-  //   //checking without department name form input field
-  //   cy.get('.form-control.form-control').eq(1).scrollIntoView().type('demo automation department description',{delay:100});
-  //   cy.get('.align-center.btn.btn-primary').scrollIntoView().click({force:true});
-  //   cy.get('.ant-notification.ant-notification-topRight.css-k7429z').should('be.visible');
+  it('check Add department',()=>
+  {
+    cy.contains('Add Department').scrollIntoView().click({force:true});
+    //checking without department name form input field
+    cy.get('.form-control.form-control').eq(1).scrollIntoView().type('demo automation department description',{delay:100});
+    cy.get('.align-center.btn.btn-primary').scrollIntoView().click({force:true});
+    cy.get('.ant-notification-notice.ant-notification-notice-info.css-12jzuas.ant-notification-notice-closable').should('be.visible');
 
-  //   cy.get('.form-control.form-control').eq(0).scrollIntoView().type('demo automation department name',{delay:100,force:true});
-  //   cy.get('.align-center.btn.btn-primary').scrollIntoView().click({force:true});
-  //   cy.contains('The department is added successfully.').should('be.visible');
-  //   cy.contains('.swal2-cancel.swal2-styled').scrollIntoView().click({force:true});
+    cy.get('.form-control.form-control').eq(0).scrollIntoView().type('demo automation department name',{delay:100,force:true});
+    cy.get('.align-center.btn.btn-primary').scrollIntoView().click({force:true});
+    cy.contains('The department is added successfully.').should('be.visible');
 
-  // })
+    cy.get('button[class="swal2-confirm swal2-styled"]').scrollIntoView().click({force:true});
+    cy.contains('tr','demo automation department name')
+    .within(()=>
+    {
+      cy.get('td:last-child')
+      .click()
+      
+    })
+    cy.contains('Delete').click();
+    cy.contains('Delete!').click();
+    cy.wait(4000);
+    //cy.wrap('table').contains('demo automation department name').should('not.exist');
+
+  })
 
 
 
 //Add Member
-// it("check Add Member Panel Ok",()=>
-// {
-//   cy.contains('Add Member').scrollIntoView().click({force:true})
-//   cy.get('.MuiInputBase-root.MuiOutlinedInput-root.MuiInputBase-colorPrimary.MuiInputBase-fullWidth.MuiInputBase-formControl.MuiInputBase-sizeSmall.MuiInputBase-adornedEnd.MuiAutocomplete-inputRoot.css-1mxnqnm').eq(0).each(($option)=>{
-//     const optionText1 = $option.text();
-//     cy.get('.MuiInputBase-root.MuiOutlinedInput-root.MuiInputBase-colorPrimary.MuiInputBase-fullWidth.MuiInputBase-formControl.MuiInputBase-sizeSmall.MuiInputBase-adornedEnd.MuiAutocomplete-inputRoot.css-1mxnqnm').eq(0).should('exist');
+it.only("check Add Member Panel Ok",()=>
+{
+  cy.wait(2000  )
+  cy.get('body > div:nth-child(2) > div:nth-child(1) > div:nth-child(3) > div:nth-child(2) > header:nth-child(2) > nav:nth-child(2) > div:nth-child(1) > div:nth-child(2) > ul:nth-child(1) > li:nth-child(4) > a:nth-child(1) > span:nth-child(2)').click({force:true});
+  cy.contains('Add Department Member').scrollIntoView().click({force:true})
+  // cy.get('.MuiInputBase-root.MuiOutlinedInput-root.MuiInputBase-colorPrimary.MuiInputBase-fullWidth.MuiInputBase-formControl.MuiInputBase-sizeSmall.MuiInputBase-adornedEnd.MuiAutocomplete-inputRoot.css-1mxnqnm').eq(0).each(($option)=>{
+  //   const optionText1 = $option.text();
+  //   cy.get('.MuiInputBase-root.MuiOutlinedInput-root.MuiInputBase-colorPrimary.MuiInputBase-fullWidth.MuiInputBase-formControl.MuiInputBase-sizeSmall.MuiInputBase-adornedEnd.MuiAutocomplete-inputRoot.css-1mxnqnm').eq(0).click({force:true});
     
 
-//   });
-//   cy.get('.MuiInputBase-root.MuiOutlinedInput-root.MuiInputBase-colorPrimary.MuiInputBase-fullWidth.MuiInputBase-formControl.MuiInputBase-sizeSmall.MuiInputBase-adornedEnd.MuiAutocomplete-inputRoot.css-1mxnqnm').eq(1).each(($option)=>{
-//     const optionText1 = $option.text();
-//     cy.get('.MuiInputBase-root.MuiOutlinedInput-root.MuiInputBase-colorPrimary.MuiInputBase-fullWidth.MuiInputBase-formControl.MuiInputBase-sizeSmall.MuiInputBase-adornedEnd.MuiAutocomplete-inputRoot.css-1mxnqnm').eq(1).should('exist');
+  // });
+  //first select
+  cy.get('.MuiInputBase-root.MuiOutlinedInput-root.MuiInputBase-colorPrimary.MuiInputBase-fullWidth.MuiInputBase-formControl.MuiInputBase-sizeSmall.MuiInputBase-adornedEnd.MuiAutocomplete-inputRoot.css-1mxnqnm')
+  .eq(0).click()
+  .get('#controllable-option-0').click({force:true});
+  //2nd select 
+  cy.get('.MuiInputBase-root.MuiOutlinedInput-root.MuiInputBase-colorPrimary.MuiInputBase-fullWidth.MuiInputBase-formControl.MuiInputBase-sizeSmall.MuiInputBase-adornedEnd.MuiAutocomplete-inputRoot.css-1mxnqnm')
+  .eq(1).click()
+  .get('#controllable-option-0').click({force:true});
+//third select
+cy.get('.MuiInputBase-root.MuiOutlinedInput-root.MuiInputBase-colorPrimary.MuiInputBase-fullWidth.MuiInputBase-formControl.MuiInputBase-sizeSmall.MuiInputBase-adornedEnd.MuiAutocomplete-inputRoot.css-1mxnqnm')
+  .eq(2).click()
+  .get('#controllable-option-0').click({force:true});
+
+  // cy.get('.MuiInputBase-root.MuiOutlinedInput-root.MuiInputBase-colorPrimary.MuiInputBase-fullWidth.MuiInputBase-formControl.MuiInputBase-sizeSmall.MuiInputBase-adornedEnd.MuiAutocomplete-inputRoot.css-1mxnqnm').eq(1).each(($option)=>{
+  //   const optionText1 = $option.text();
+  //   cy.get('.MuiInputBase-root.MuiOutlinedInput-root.MuiInputBase-colorPrimary.MuiInputBase-fullWidth.MuiInputBase-formControl.MuiInputBase-sizeSmall.MuiInputBase-adornedEnd.MuiAutocomplete-inputRoot.css-1mxnqnm').eq(1).click({force:true});
     
 
-//   });
-//   cy.get('.MuiInputBase-root.MuiOutlinedInput-root.MuiInputBase-colorPrimary.MuiInputBase-fullWidth.MuiInputBase-formControl.MuiInputBase-sizeSmall.MuiInputBase-adornedEnd.MuiAutocomplete-inputRoot.css-1mxnqnm').eq(2).each(($option)=>{
-//     const optionText1 = $option.text();
-//     cy.get('.MuiInputBase-root.MuiOutlinedInput-root.MuiInputBase-colorPrimary.MuiInputBase-fullWidth.MuiInputBase-formControl.MuiInputBase-sizeSmall.MuiInputBase-adornedEnd.MuiAutocomplete-inputRoot.css-1mxnqnm').eq(2).should('exist');
+  // });
+  // cy.get('.MuiInputBase-root.MuiOutlinedInput-root.MuiInputBase-colorPrimary.MuiInputBase-fullWidth.MuiInputBase-formControl.MuiInputBase-sizeSmall.MuiInputBase-adornedEnd.MuiAutocomplete-inputRoot.css-1mxnqnm').eq(2).each(($option)=>{
+  //   const optionText1 = $option.text();
+  //   cy.get('.MuiInputBase-root.MuiOutlinedInput-root.MuiInputBase-colorPrimary.MuiInputBase-fullWidth.MuiInputBase-formControl.MuiInputBase-sizeSmall.MuiInputBase-adornedEnd.MuiAutocomplete-inputRoot.css-1mxnqnm').eq(2).click({force:true});
     
 
-//   });
-//   cy.get('.align-center.btn.btn-primary').scrollIntoView().click({force:true});
-//   cy.contains('department member with this Member already exists.').scrollIntoView().should('be.visible');
-// })
+  // });
+  cy.get('.align-center.btn.btn-primary').scrollIntoView().click({force:true});
+  cy.contains('department member with this Member already exists.').scrollIntoView().should('be.visible');
+})
 
 
 
 
 //Department members
 
-// it('should validate table rows and edit designation panel', () => {
+it('should validate table rows and edit designation panel', () => {
   
-//   cy.contains('Department Members').click({force:true})
-//   // Get all table rows
-//   cy.get('table tbody tr').each(($row) => {
-//     // Validate member name column
-//     cy.wrap($row).find('td:nth-child(1)').should('not.be.empty');
+  cy.contains('Department Members').click({force:true})
+  // Get all table rows
+  cy.get('table tbody tr').each(($row) => {
+    // Validate member name column
+    cy.wrap($row).find('td:nth-child(1)').should('not.be.empty');
 
-//     // Validate department name column
-//     cy.wrap($row).find('td:nth-child(2)').should('not.be.empty');
+    // Validate department name column
+    cy.wrap($row).find('td:nth-child(2)').should('not.be.empty');
 
-//     // Validate designation column
-//     cy.wrap($row).find('td:nth-child(3)').should('not.be.empty');
-
-
-//     cy.wrap($row).find('td:nth-child(4)').should('not.be.empty');
+    // Validate designation column
+    cy.wrap($row).find('td:nth-child(3)').should('not.be.empty');
 
 
-//     // Validate edit designation panel
-//     cy.wrap($row)
-//       .find('td:nth-child(5)')
-//       .within(() => {
-//         // Check for Edit button
-//         cy.get('.m-r-5.btn-success.m-b-10').should('exist');
-//         cy.get('.m-r-5.btn-success.m-b-10').scrollIntoView().click({force:true});
+    cy.wrap($row).find('td:nth-child(4)').should('not.be.empty');
+
+
+    // Validate edit designation panel
+    cy.wrap($row)
+      .find('td:nth-child(5)')
+      .within(() => {
+        // Check for Edit button
+        cy.get('.m-r-5.btn-success.m-b-10').should('exist');
+        cy.get('.m-r-5.btn-success.m-b-10').scrollIntoView().click({force:true});
         
-//         cy.get('.m-r-5.btn-success.m-b-10').click({force:true});
+        cy.get('.m-r-5.btn-success.m-b-10').click({force:true});
 
 
 
-//         // Check for Delete button
-//         cy.get('.m-r-5.btn-success.m-b-10').should('exist');
-//       });
-//   });
-// });
+        // Check for Delete button
+        cy.get('.m-r-5.btn-success.m-b-10').should('exist');
+      });
+  });
+});
 
 
 //Add designation
